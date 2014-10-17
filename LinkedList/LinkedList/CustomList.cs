f4d5cc806dbe97f21309fe3c3ce007ec448ade0a
atanner27 <atanner2727@gmail.com>
2014-10-17 17:12:43 -0400

    Creating Practice linked list in c#
    To be used for future problem solving practice

---
+++ LinkedList/LinkedList/CustomList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class CustomList
+    {
+        private Element Root;
+
+        public void AddFront(int value)
+        {
+            Element newElement = new Element();
+            newElement.Value = value;
+
+            //move list
+            Element OldRoot = Root;
+            Root = newElement;
+            newElement.Next = OldRoot;
+        }
+
+        public void AddBack(int value)
+        {
+            Element newElement = new Element();
+            newElement.Value = value;
+
+            if (Root == null)
+            {
+                Root = newElement;
+            }
+            else
+            {
+
+
+                //loop through list until not null
+                Element Temp = Root;
+                while (Temp.Next != null)
+                {
+                    Temp = Temp.Next;
+                }
+                //Assign the new val
+                Temp.Next = newElement;
+
+            }
+        }
+
+        public void PrintList()
+        {
+            Element Temp = Root;
+            while (Temp != null)
+            {
+                System.Console.WriteLine(Temp.Value);
+                Temp = Temp.Next;
+            }
+            
+        }
+    }
+
+    public class Element
+    {
+        public int Value;
+        public Element Next;
+    }
+
+   
+}
---
+++ LinkedList/ListTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ListTest;
+using LinkedList;
+namespace ListTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CustomList List = new CustomList();
+
+            
+
+            List.PrintList();
+            Console.ReadLine();
+        }
+
+        static void TestAddBack(CustomList List)
+        {
+            List.AddBack(1);
+            List.AddBack(2);
+            List.AddBack(3);
+            List.AddBack(4);
+            List.AddBack(5);
+        }
+
+        static void TestAddFront(CustomList List)
+        {
+            List.AddFront(1);
+            List.AddFront(2);
+            List.AddFront(3);
+            List.AddFront(4);
+            List.AddFront(5);
+        }
+
+    }
+}
